163d52f40218c1fef55c02df1a23d82aa67552ac
144bharat <144bharatgautam@gmail.com>
2025-03-02 19:10:15 +0530

    Upgraded application to v18

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,7 @@ import { provideRouter, withViewTransitions } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { routes } from './app.routes';
 
-import {provideHttpClient} from '@angular/common/http';
+import { provideHttpClient } from '@angular/common/http';
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes,withViewTransitions()),provideAnimations(),provideHttpClient()]
 };
